09073bd23a4a5e985eeb8528e9f0f661967a2dfe
teohteiktoe-glitch <teohteiktoe@gmail.com>
2026-09-16 12:30:15 +0000

    1

--- app.py
+++ app.py
@@ -8,6 +8,14 @@ app = Flask(__name__)
 def index():
     return(render_template("index.html"))
 
+@app.route("/main",methods=["GET","POST"])
+def main():
+    return(render_template("main.html"))
+
+@app.route("/transferMoney",methods=["GET","POST"])
+def transferMoney():
+    return(render_template("transferMoney.html"))
+
 if __name__ == "__main__":
     app.run()
 
